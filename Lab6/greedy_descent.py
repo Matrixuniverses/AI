2b755120460c7470258f20fdd5e5782c30d31d04
Sam Shankland <sjs227@uclive.ac.nz>
2019-09-22 20:34:40 +1200

    Added Q4

--- Lab6/greedy_descent.py
+++ Lab6/greedy_descent.py
@@ -1,3 +1,8 @@
+import math
+import random
+from n_queens_neighbours import n_queens_neighbours as neighbours
+from n_queens_neighbours import n_queens_cost as cost
+
 def greedy_descent(start_state, neighbours, cost):
     states_visited, current_state, select = [start_state], start_state, True
     yield current_state
@@ -17,5 +22,14 @@ def greedy_descent(start_state, neighbours, cost):
             yield current_state
 
 def greedy_descent_with_random_restart(random_state, neighbours, cost):
-    print("ligma")
+    g_min, l_min = 0, math.inf
+    
+    while l_min > g_min:
+        states = list(greedy_descent(random_state(), neighbours, cost))
+        l_min = min(cost(states) for states in states)
+
+        print(*states, sep="\n")
+        
+        if l_min > g_min: print('RESTART', end="\n")
+
 
